3bfcbabe378af88ddf2f32ac9e44008cfff48d7d
LABORATORIO-1 <LABORATORIO-1@DESKTOP-38H5AJS>
2024-08-21 10:07:18 -0600

    Agregar archivos de proyecto.

---
+++ CalculoIMC/Program.cs
@@ -0,0 +1,17 @@
+﻿// Declaración de variables
+double peso;
+double altura;
+double imc;
+
+// Solicitar datos al usuario
+Console.WriteLine("Ingrese su peso en kilogramos:");
+peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese su altura en metros:");
+altura = Convert.ToDouble(Console.ReadLine());
+
+// Calcular el IMC
+imc = peso / (altura * altura);
+
+// Mostrar el IMC calculado
+Console.WriteLine($"Su IMC es: {imc:F}");
