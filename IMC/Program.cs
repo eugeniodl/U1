6317335500414848abd6e31f39357d341d03701f
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-08-21 10:13:57 -0600

    Agregar proyecto.

--- IMC/Program.cs
+++ IMC/Program.cs
@@ -4,14 +4,16 @@ double altura;
 double imc;
 
 // Solicitar datos al usuario
-Console.WriteLine("Ingrese su peso en kilogramos:");
+Console.WriteLine("Ingrese su peso en kilogramos");
 peso = Convert.ToDouble(Console.ReadLine());
 
-Console.WriteLine("Ingrese su altura en metros:");
+Console.WriteLine("Ingrese su altura en metros");
 altura = Convert.ToDouble(Console.ReadLine());
 
 // Calcular el IMC
 imc = peso / (altura * altura);
 
 // Mostrar el IMC calculado
-Console.WriteLine($"Su IMC es: {imc:F}");
+Console.WriteLine($"Su IMC es: {imc:F2}");
+
+Console.ReadLine();
