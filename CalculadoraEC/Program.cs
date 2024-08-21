5adc2c28aed6a00dc4988577a849083d06a58211
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-08-21 15:05:32 -0600

    CalculadoraEC

--- CalculadoraEC/Program.cs
+++ CalculadoraEC/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Declaración de variables
+double masa, velocidad, energiaCinetica;
+
+// Solicitar la masa y la velocidad
+Console.WriteLine("Ingrese la masa del objeto en kilogramos:");
+masa = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese la velocidad del objeto en metros por segundo:");
+velocidad = Convert.ToDouble(Console.ReadLine());
+
+// Calcular la energía cinética
+energiaCinetica = 1.0 / 2.0 * masa * Math.Pow(velocidad, 2);
+
+// Mostrar el resultado de la energía cinética
+Console.WriteLine($"La energía cinética del objeto es: {energiaCinetica:F2} joules");
