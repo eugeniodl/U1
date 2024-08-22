dcc72b92d32355957ec6254c11389a5ea3ed92ad
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-08-22 14:10:04 -0600

    Estructuras de control.

---
+++ CalculoIMC/Program.cs
@@ -0,0 +1,34 @@
+﻿// Declaración de variables
+double peso;
+double altura;
+double imc;
+
+// Solicitar datos al usuario
+Console.WriteLine("Ingrese su peso en kilogramos:");
+peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese su altura en metros:");
+altura = Convert.ToDouble(Console.ReadLine());
+
+// Calcular el IMC
+imc = peso / (altura * altura);
+
+// Mostrar el IMC calculado
+Console.WriteLine($"Su IMC es: {imc:F}");
+
+// Determinar el rango del IMC
+if(imc < 18.5)
+{
+    Console.WriteLine("Peso bajo.");
+}
+else if(imc >= 18.5 && imc <= 24.9)
+{
+    Console.WriteLine("Peso normal.");
+}
+else
+{
+    Console.WriteLine("Sobrepeso.");
+}
+
+Console.ReadLine();
+
