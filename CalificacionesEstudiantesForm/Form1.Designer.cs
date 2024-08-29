5bed4d8f67117a8c136a4ec29195185aa3575d66
eugeniodl <Eugenio.Diaz@fcys.uni.edu.ni>
2024-08-29 14:16:42 -0600

    CalificacionesEstudiantesForm

---
+++ CalificacionesEstudiantesForm/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace CalificacionesEstudiantesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            txtEst1Ex1 = new TextBox();
+            txtEst1Ex2 = new TextBox();
+            txtEst1Ex3 = new TextBox();
+            txtEst2Ex3 = new TextBox();
+            txtEst2Ex2 = new TextBox();
+            txtEst2Ex1 = new TextBox();
+            txtEst3Ex3 = new TextBox();
+            txtEst3Ex2 = new TextBox();
+            txtEst3Ex1 = new TextBox();
+            lblPromedioGeneral = new Label();
+            btnCalcularPromedios = new Button();
+            lstPromedios = new ListBox();
+            txtEst4Ex3 = new TextBox();
+            txtEst4Ex2 = new TextBox();
+            txtEst4Ex1 = new TextBox();
+            label9 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(181, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(476, 38);
+            label1.TabIndex = 0;
+            label1.Text = "CALIFICACIONES DE ESTUDIANTES";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(232, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Ex1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(337, 62);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Ex2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(439, 62);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 28);
+            label4.TabIndex = 3;
+            label4.Text = "Ex3";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(60, 116);
+            label5.Name = "label5";
+            label5.Size = new Size(135, 28);
+            label5.TabIndex = 4;
+            label5.Text = "Estudiante 1:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(60, 169);
+            label6.Name = "label6";
+            label6.Size = new Size(135, 28);
+            label6.TabIndex = 5;
+            label6.Text = "Estudiante 2:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(60, 222);
+            label7.Name = "label7";
+            label7.Size = new Size(135, 28);
+            label7.TabIndex = 6;
+            label7.Text = "Estudiante 3:";
+            // 
+            // txtEst1Ex1
+            // 
+            txtEst1Ex1.Location = new Point(232, 116);
+            txtEst1Ex1.Name = "txtEst1Ex1";
+            txtEst1Ex1.Size = new Size(50, 27);
+            txtEst1Ex1.TabIndex = 7;
+            // 
+            // txtEst1Ex2
+            // 
+            txtEst1Ex2.Location = new Point(337, 116);
+            txtEst1Ex2.Name = "txtEst1Ex2";
+            txtEst1Ex2.Size = new Size(50, 27);
+            txtEst1Ex2.TabIndex = 8;
+            // 
+            // txtEst1Ex3
+            // 
+            txtEst1Ex3.Location = new Point(439, 116);
+            txtEst1Ex3.Name = "txtEst1Ex3";
+            txtEst1Ex3.Size = new Size(50, 27);
+            txtEst1Ex3.TabIndex = 9;
+            // 
+            // txtEst2Ex3
+            // 
+            txtEst2Ex3.Location = new Point(439, 169);
+            txtEst2Ex3.Name = "txtEst2Ex3";
+            txtEst2Ex3.Size = new Size(50, 27);
+            txtEst2Ex3.TabIndex = 12;
+            // 
+            // txtEst2Ex2
+            // 
+            txtEst2Ex2.Location = new Point(337, 169);
+            txtEst2Ex2.Name = "txtEst2Ex2";
+            txtEst2Ex2.Size = new Size(50, 27);
+            txtEst2Ex2.TabIndex = 11;
+            // 
+            // txtEst2Ex1
+            // 
+            txtEst2Ex1.Location = new Point(232, 169);
+            txtEst2Ex1.Name = "txtEst2Ex1";
+            txtEst2Ex1.Size = new Size(50, 27);
+            txtEst2Ex1.TabIndex = 10;
+            // 
+            // txtEst3Ex3
+            // 
+            txtEst3Ex3.Location = new Point(439, 222);
+            txtEst3Ex3.Name = "txtEst3Ex3";
+            txtEst3Ex3.Size = new Size(50, 27);
+            txtEst3Ex3.TabIndex = 15;
+            // 
+            // txtEst3Ex2
+            // 
+            txtEst3Ex2.Location = new Point(337, 222);
+            txtEst3Ex2.Name = "txtEst3Ex2";
+            txtEst3Ex2.Size = new Size(50, 27);
+            txtEst3Ex2.TabIndex = 14;
+            // 
+            // txtEst3Ex1
+            // 
+            txtEst3Ex1.Location = new Point(232, 222);
+            txtEst3Ex1.Name = "txtEst3Ex1";
+            txtEst3Ex1.Size = new Size(50, 27);
+            txtEst3Ex1.TabIndex = 13;
+            // 
+            // lblPromedioGeneral
+            // 
+            lblPromedioGeneral.AutoSize = true;
+            lblPromedioGeneral.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblPromedioGeneral.Location = new Point(249, 336);
+            lblPromedioGeneral.Name = "lblPromedioGeneral";
+            lblPromedioGeneral.Size = new Size(294, 28);
+            lblPromedioGeneral.TabIndex = 21;
+            lblPromedioGeneral.Text = "Promedio General de la Clase:";
+            // 
+            // btnCalcularPromedios
+            // 
+            btnCalcularPromedios.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            btnCalcularPromedios.Location = new Point(547, 65);
+            btnCalcularPromedios.Name = "btnCalcularPromedios";
+            btnCalcularPromedios.Size = new Size(180, 51);
+            btnCalcularPromedios.TabIndex = 19;
+            btnCalcularPromedios.Text = "Calcular Promedios";
+            btnCalcularPromedios.UseVisualStyleBackColor = true;
+            btnCalcularPromedios.Click += btnCalcularPromedios_Click;
+            // 
+            // lstPromedios
+            // 
+            lstPromedios.FormattingEnabled = true;
+            lstPromedios.ItemHeight = 20;
+            lstPromedios.Location = new Point(547, 139);
+            lstPromedios.Name = "lstPromedios";
+            lstPromedios.Size = new Size(261, 164);
+            lstPromedios.TabIndex = 20;
+            // 
+            // txtEst4Ex3
+            // 
+            txtEst4Ex3.Location = new Point(439, 275);
+            txtEst4Ex3.Name = "txtEst4Ex3";
+            txtEst4Ex3.Size = new Size(50, 27);
+            txtEst4Ex3.TabIndex = 18;
+            // 
+            // txtEst4Ex2
+            // 
+            txtEst4Ex2.Location = new Point(337, 275);
+            txtEst4Ex2.Name = "txtEst4Ex2";
+            txtEst4Ex2.Size = new Size(50, 27);
+            txtEst4Ex2.TabIndex = 17;
+            // 
+            // txtEst4Ex1
+            // 
+            txtEst4Ex1.Location = new Point(232, 275);
+            txtEst4Ex1.Name = "txtEst4Ex1";
+            txtEst4Ex1.Size = new Size(50, 27);
+            txtEst4Ex1.TabIndex = 16;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(60, 275);
+            label9.Name = "label9";
+            label9.Size = new Size(135, 28);
+            label9.TabIndex = 19;
+            label9.Text = "Estudiante 4:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(839, 406);
+            Controls.Add(txtEst4Ex3);
+            Controls.Add(txtEst4Ex2);
+            Controls.Add(txtEst4Ex1);
+            Controls.Add(label9);
+            Controls.Add(lstPromedios);
+            Controls.Add(btnCalcularPromedios);
+            Controls.Add(lblPromedioGeneral);
+            Controls.Add(txtEst3Ex3);
+            Controls.Add(txtEst3Ex2);
+            Controls.Add(txtEst3Ex1);
+            Controls.Add(txtEst2Ex3);
+            Controls.Add(txtEst2Ex2);
+            Controls.Add(txtEst2Ex1);
+            Controls.Add(txtEst1Ex3);
+            Controls.Add(txtEst1Ex2);
+            Controls.Add(txtEst1Ex1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox txtEst1Ex1;
+        private TextBox txtEst1Ex2;
+        private TextBox txtEst1Ex3;
+        private TextBox txtEst2Ex3;
+        private TextBox txtEst2Ex2;
+        private TextBox txtEst2Ex1;
+        private TextBox txtEst3Ex3;
+        private TextBox txtEst3Ex2;
+        private TextBox txtEst3Ex1;
+        private Label lblPromedioGeneral;
+        private Button btnCalcularPromedios;
+        private ListBox lstPromedios;
+        private TextBox txtEst4Ex3;
+        private TextBox txtEst4Ex2;
+        private TextBox txtEst4Ex1;
+        private Label label9;
+    }
+}
